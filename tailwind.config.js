00939e43fff09c2913029a6a4f2f0f0fcef8e264
Yazeed Bzadough <ybzadough@gmail.com>
2021-05-05 23:25:18 -0400

    Homepage tailwind-ified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    colors: {
+      white: '#ffffff',
+      black: '#000',
+
+      'blue-1': '#03449e',
+      'blue-2': '#01337d',
+
+      'neutral-1': '#1f2933',
+      'neutral-2': '#3e4c59',
+      'neutral-3': '#e4e7eb',
+      'neutral-4': '#f5f7fa',
+
+      'supporting-1': '#05606e',
+      'supporting-2': '#f7c948',
+      'supporting-3': '#ad1d07'
+
+      // 'box-shadow-1': '1px 1px 2px rgba(0, 0, 0, 0.4)',
+      // 'box-shadow-2': '0px 3px 6px rgba(0, 0, 0, 0.15)'
+    },
+    fontFamily: {
+      sans: 'Avenir, Arial, Helvetica, sans-serif'
+    }
   },
   variants: {
     extend: {}
